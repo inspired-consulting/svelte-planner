a50049271d4b6e5fbab30ff49d11d82815c9ad9d
Fenja Pestke <fenja.pestke@gmail.com>
2020-08-31 10:03:49 +0200

    create project-page

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const pug = new Pug({
 router
     .get('/', ctx => ctx.render('dashboard'))
     .get('/person', ctx => ctx.render('person'))
+    .get('/project', ctx => ctx.render('project'))
 
 app.use(router.routes());
 
